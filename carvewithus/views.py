b6826c9d588bea6a293d368513072da330cf08bb
Ben Hu <benzheren@gmail.com>
2011-03-07 23:48:11 -0800

    change dev settings, put facebook id secret into settings. put extra dependency into setup.py

--- carvewithus/views.py
+++ carvewithus/views.py
@@ -1,7 +1,66 @@
-from carvewithus.models import DBSession
-from carvewithus.models import User
+import facebook
 
-def my_view(request):
-    dbsession = DBSession()
-    root = dbsession.query(User).filter(User.name==u'Ben Hu').first()
-    return {'root':root, 'project':'carvewithus'}
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+from pyramid.response import Response
+from pyramid.url import route_url
+from formalchemy import validators
+from formalchemy.fields import Field
+from formalchemy.forms import FieldSet
+from sqlalchemy import func
+
+from carvewithus.models import DBSession, User
+
+FACEBOOK_APP_ID = "145582475506407"
+FACEBOOK_APP_SECRET = "faff3a09153017e393783346c3dec187"
+
+
+class SignUp(object):
+    """docstring for SignUp"""
+    def __init__(self, request):
+       self.request = request
+       self.dbsession = DBSession()
+    
+    @view_config(route_name="signup", renderer='signup.mak')
+    def signup(self):
+        result = {'facebook_app_id' : FACEBOOK_APP_ID}
+        cookie = facebook.get_user_from_cookie(self.request.cookies, 
+                                               FACEBOOK_APP_ID,
+                                               FACEBOOK_APP_SECRET)
+        if cookie:
+            graph = facebook.GraphAPI(cookie["access_token"])
+            profile = graph.get_object("me")
+            result['profile_url'] = profile["link"]
+            result['id'] = profile["id"]
+        else:
+            result['id'] = None
+
+        step_0 = FieldSet(User)
+        step_0.configure(
+            include = [
+                step_0.name,
+                step_0.email,
+                step_0.password,
+            ],
+            options=[step_0.email.set(validate=validators.email)]
+        )
+        fs = step_0.bind(User, data=self.request.POST or None)
+        if self.request.POST and fs.validate():
+            fs.sync()
+            count = self.dbsession.query(func.count('*').label('count')).filter(
+                        User.name==fs.name.value).first().count
+            if count == 0:
+                fs.model.username = fs.name.value
+            else:
+                fs.model.username = fs.name.value + "-%d" % count
+            self.dbsession.add(fs.model)
+            self.dbsession.commit()
+            return HTTPFound(location=route_url('create_profile', self.request))
+
+        result['fs'] = fs
+        return result
+
+
+    @view_config(route_name="create_profile", renderer='create_profile.mak')
+    def create_profile(request):
+        return {'root':'hello'}
--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ requires = [
     'repoze.tm2>=1.0b1', # default_commit_veto
     'zope.sqlalchemy',
     'WebError',
+    'facebook-python-sdk',
+    'FormAlchemy',
     ]
 
 if sys.version_info[:3] < (2,5,0):
